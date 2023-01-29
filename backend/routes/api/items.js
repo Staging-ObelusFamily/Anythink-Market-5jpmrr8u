ac4eff07df19a90cf128da14de7376ec5d20a1b4
WilcoApp <hello@wilco.gg>
2023-01-29 21:19:15 +0200

    Robocrop

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -143,7 +143,7 @@ router.post("/", auth.required, function(req, res, next) {
       if (!user) {
         return res.sendStatus(401);
       }
-req.body.item.description = "dsdsfdsf"
+
       var item = new Item(req.body.item);
 
       item.seller = user;
